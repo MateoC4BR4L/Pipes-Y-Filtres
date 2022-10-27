8456643d9a0041151b6fbd48fda4d99a392c84d7
MateoC4BR4L <juan.cabral@correo.ucu.edu.uy>
2022-10-27 18:02:44 -0300

    Ejercicio 1 completado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,21 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            Picture newPicture = new Picture(30, 40);
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"PathToImageToLoad.jpg");
+
+            provider.SavePicture(newPicture, @"PathToImageToSave.jpg");
+
+            PipeNull nullpipe = new PipeNull();
+            FilterNegative filtroNegativo = new FilterNegative();
+            PipeSerial pipeSerial2 = new PipeSerial(filtroNegativo, nullpipe);
+            
+            FilterGreyscale filtroGris = new FilterGreyscale();
+            PipeSerial piepserial1 = new PipeSerial(filtroGris, pipeSerial2);
+
+            piepserial1.Send(newPicture);
         }
     }
 }
